89921a532b89aa8ab7168168695e179255799e9c
al1-ce <al1-ce@null.net>
2024-10-17 01:57:07 +0300

    better? replac_all

--- lua/jsfunc/init.lua
+++ lua/jsfunc/init.lua
@@ -1,163 +1,446 @@
-require("jsfunc.luaspecial")
+[
+  Node {
+    type: 'ExpressionStatement',
+    start: 56,
+    end: 84,
+    loc: SourceLocation {
+      start: [Position],
+      end: [Position],
+      filename: undefined,
+      identifierName: undefined
+    },
+    expression: Node {
+      type: 'CallExpression',
+      start: 56,
+      end: 84,
+      loc: [SourceLocation],
+      callee: [Node],
+      arguments: [Array]
+    },
+    leadingComments: [ [Object] ]
+  },
+  Node {
+    type: 'FunctionDeclaration',
+    start: 86,
+    end: 291,
+    loc: SourceLocation {
+      start: [Position],
+      end: [Position],
+      filename: undefined,
+      identifierName: undefined
+    },
+    id: Node {
+      type: 'Identifier',
+      start: 95,
+      end: 108,
+      loc: [SourceLocation],
+      name: 'iter_to_array'
+    },
+    generator: false,
+    async: false,
+    params: [ [Node] ],
+    body: Node {
+      type: 'BlockStatement',
+      start: 115,
+      end: 291,
+      loc: [SourceLocation],
+      body: [Array],
+      directives: []
+    }
+  },
+  Node {
+    type: 'ExpressionStatement',
+    start: 295,
+    end: 572,
+    loc: SourceLocation {
+      start: [Position],
+      end: [Position],
+      filename: undefined,
+      identifierName: undefined
+    },
+    expression: Node {
+      type: 'AssignmentExpression',
+      start: 295,
+      end: 571,
+      loc: [SourceLocation],
+      operator: '=',
+      left: [Node],
+      right: [Node]
+    }
+  },
+  Node {
+    type: 'ExpressionStatement',
+    start: 574,
+    end: 672,
+    loc: SourceLocation {
+      start: [Position],
+      end: [Position],
+      filename: undefined,
+      identifierName: undefined
+    },
+    expression: Node {
+      type: 'AssignmentExpression',
+      start: 574,
+      end: 672,
+      loc: [SourceLocation],
+      operator: '=',
+      left: [Node],
+      right: [Node]
+    }
+  },
+  Node {
+    type: 'ExpressionStatement',
+    start: 674,
+    end: 778,
+    loc: SourceLocation {
+      start: [Position],
+      end: [Position],
+      filename: undefined,
+      identifierName: undefined
+    },
+    expression: Node {
+      type: 'AssignmentExpression',
+      start: 674,
+      end: 778,
+      loc: [SourceLocation],
+      operator: '=',
+      left: [Node],
+      right: [Node]
+    }
+  },
+  Node {
+    type: 'ExpressionStatement',
+    start: 780,
+    end: 897,
+    loc: SourceLocation {
+      start: [Position],
+      end: [Position],
+      filename: undefined,
+      identifierName: undefined
+    },
+    expression: Node {
+      type: 'AssignmentExpression',
+      start: 780,
+      end: 897,
+      loc: [SourceLocation],
+      operator: '=',
+      left: [Node],
+      right: [Node]
+    }
+  },
+  Node {
+    type: 'ExpressionStatement',
+    start: 899,
+    end: 1226,
+    loc: SourceLocation {
+      start: [Position],
+      end: [Position],
+      filename: undefined,
+      identifierName: undefined
+    },
+    expression: Node {
+      type: 'AssignmentExpression',
+      start: 899,
+      end: 1226,
+      loc: [SourceLocation],
+      operator: '=',
+      left: [Node],
+      right: [Node]
+    }
+  },
+  Node {
+    type: 'ExpressionStatement',
+    start: 1228,
+    end: 1337,
+    loc: SourceLocation {
+      start: [Position],
+      end: [Position],
+      filename: undefined,
+      identifierName: undefined
+    },
+    expression: Node {
+      type: 'AssignmentExpression',
+      start: 1228,
+      end: 1337,
+      loc: [SourceLocation],
+      operator: '=',
+      left: [Node],
+      right: [Node]
+    }
+  },
+  Node {
+    type: 'ExpressionStatement',
+    start: 1339,
+    end: 1450,
+    loc: SourceLocation {
+      start: [Position],
+      end: [Position],
+      filename: undefined,
+      identifierName: undefined
+    },
+    expression: Node {
+      type: 'AssignmentExpression',
+      start: 1339,
+      end: 1450,
+      loc: [SourceLocation],
+      operator: '=',
+      left: [Node],
+      right: [Node]
+    }
+  },
+  Node {
+    type: 'ExpressionStatement',
+    start: 1452,
+    end: 1606,
+    loc: SourceLocation {
+      start: [Position],
+      end: [Position],
+      filename: undefined,
+      identifierName: undefined
+    },
+    expression: Node {
+      type: 'AssignmentExpression',
+      start: 1452,
+      end: 1606,
+      loc: [SourceLocation],
+      operator: '=',
+      left: [Node],
+      right: [Node]
+    },
+    trailingComments: [ [Object], [Object], [Object], [Object] ]
+  },
+  Node {
+    type: 'ExpressionStatement',
+    start: 1764,
+    end: 1822,
+    loc: SourceLocation {
+      start: [Position],
+      end: [Position],
+      filename: undefined,
+      identifierName: undefined
+    },
+    expression: Node {
+      type: 'AssignmentExpression',
+      start: 1764,
+      end: 1822,
+      loc: [SourceLocation],
+      operator: '=',
+      left: [Node],
+      right: [Node]
+    },
+    trailingComments: [ [Object] ],
+    leadingComments: [ [Object], [Object], [Object], [Object] ]
+  },
+  Node {
+    type: 'ExpressionStatement',
+    start: 1849,
+    end: 1902,
+    loc: SourceLocation {
+      start: [Position],
+      end: [Position],
+      filename: undefined,
+      identifierName: undefined
+    },
+    expression: Node {
+      type: 'AssignmentExpression',
+      start: 1849,
+      end: 1902,
+      loc: [SourceLocation],
+      operator: '=',
+      left: [Node],
+      right: [Node]
+    },
+    leadingComments: [ [Object] ]
+  },
+  Node {
+    type: 'ExpressionStatement',
+    start: 1904,
+    end: 2158,
+    loc: SourceLocation {
+      start: [Position],
+      end: [Position],
+      filename: undefined,
+      identifierName: undefined
+    },
+    expression: Node {
+      type: 'AssignmentExpression',
+      start: 1904,
+      end: 2158,
+      loc: [SourceLocation],
+      operator: '=',
+      left: [Node],
+      right: [Node]
+    }
+  },
+  Node {
+    type: 'ExpressionStatement',
+    start: 2160,
+    end: 2413,
+    loc: SourceLocation {
+      start: [Position],
+      end: [Position],
+      filename: undefined,
+      identifierName: undefined
+    },
+    expression: Node {
+      type: 'AssignmentExpression',
+      start: 2160,
+      end: 2413,
+      loc: [SourceLocation],
+      operator: '=',
+      left: [Node],
+      right: [Node]
+    }
+  },
+  Node {
+    type: 'ExpressionStatement',
+    start: 2415,
+    end: 2697,
+    loc: SourceLocation {
+      start: [Position],
+      end: [Position],
+      filename: undefined,
+      identifierName: undefined
+    },
+    expression: Node {
+      type: 'AssignmentExpression',
+      start: 2415,
+      end: 2697,
+      loc: [SourceLocation],
+      operator: '=',
+      left: [Node],
+      right: [Node]
+    }
+  }
+]
+require("jsfunc.luaspecial");
 local function iter_to_array(iter)
-    local arr = {}
-    local item = nil
-    while true do
-        item = iter()
-        if item == nil then
-            break
-        end
-        table.insert(arr, item)
-    end
-    return arr
-end
+	local arr = {};
+	local item = nil;
+	while true do
+		item = iter();
+		if item == nil then
+			break
+		end;
+		table.insert(arr, item)
+	end;
+	return arr
+end;
 function string:split(separator)
-    local t = {}
-    local items =
-        iter_to_array(
-        (function()
-            local __tmp = self
-            return __tmp:gmatch(string.format([=[[^%s]+]=], separator))
-        end)()
-    )
-    do
-        local i = 0
-        while i < #items do
-            table.insert(t, items[i + 1])
-            local _null
-            (function()
-                i = i + 1
-                return i
-            end)()
-        end
-    end
-    return t
-end
+	local t = {};
+	local items = iter_to_array((function()
+		local __tmp = self
+		return __tmp:gmatch(string.format([=[[^%s]+]=], separator))
+	end)());
+	do
+		local i = 0
+		while i < # items do
+			table.insert(t, items[i + 1]);
+			local _null
+			i = i + 1
+		end
+	end;
+	return t
+end;
 function string:contains(pattern)
-    return (function()
-        local __tmp = self
-        return __tmp:find(pattern, 1, true)
-    end)() ~= nil
-end
+	return (function()
+		local __tmp = self
+		return __tmp:find(pattern, 1, true)
+	end)() ~= nil
+end;
 function string:starts_with(pattern)
-    return (function()
-        local __tmp = self
-        return __tmp:sub(1, #pattern)
-    end)() == pattern
-end
+	return (function()
+		local __tmp = self
+		return __tmp:sub(1, # pattern)
+	end)() == pattern
+end;
 function string:ends_with(pattern)
-    return pattern == "" or (function()
-            local __tmp = self
-            return __tmp:sub(-(#pattern))
-        end)() == pattern
-end
+	return pattern == "" or (function()
+		local __tmp = self
+		return __tmp:sub(- # pattern)
+	end)() == pattern
+end;
 function string:replace(pattern, replacement)
-    local str = self
-    local search_start_idx = 1
-    local idx = pack2(str:find(pattern, search_start_idx, true))
-    if not idx[1] then
-        return str
-    end
-    local postfix = str:sub(idx[2] + 1)
-    str = string.format([=[%s%s%s]=], str:sub(1, idx[1] - 1), replacement, postfix)
-    return str
-end
+	local str = self;
+	local search_start_idx = 1;
+	local idx = pack2(str:find(pattern, search_start_idx, true));
+	if not idx[1] then
+		return str
+	end;
+	local postfix = str:sub(idx[2] + 1);
+	str = string.format([=[%s%s%s]=], str:sub(1, idx[1] - 1), replacement, postfix);
+	return str
+end;
 function string:replace_all(pattern, replacement)
-    local str = self
-    local search_start_idx = 1
-    while true do
-        local idx = pack2(str:find(pattern, search_start_idx, true))
-        if not idx[1] then
-            break
-        end
-        local postfix = str:sub(idx[2] + 1)
-        str = string.format([=[%s%s%s]=], str:sub(1, idx[1] - 1), replacement, postfix)
-        search_start_idx = -1 * postfix:len()
-    end
-    return str
-end
+	return (function()
+		local __tmp = self
+		return __tmp:gsub(pattern, replacement)
+	end)()
+end;
 function string:insert(pos, text)
-    return string.format(
-        [=[%s%s%s]=],
-        (function()
-            local __tmp = self
-            return __tmp:sub(1, pos - 1)
-        end)(),
-        text,
-        (function()
-            local __tmp = self
-            return __tmp:sub(pos)
-        end)()
-    )
-end
+	return string.format([=[%s%s%s]=], (function()
+		local __tmp = self
+		return __tmp:sub(1, pos - 1)
+	end)(), text, (function()
+		local __tmp = self
+		return __tmp:sub(pos)
+	end)())
+end;
 string["repeat"] = function(self, count)
-    local out = ""
-    do
-        local i = 0
-        while i < count do
-            out = string.format([=[%s%s]=], out, self)
-            local _null
-            (function()
-                i = i + 1
-                return i
-            end)()
-        end
-    end
-    return out
-end
+	local out = "";
+	do
+		local i = 0
+		while i < count do
+			out = string.format([=[%s%s]=], out, self);
+			local _null
+			i = i + 1
+		end
+	end;
+	return out
+end;
 table.shift = function(tbl)
-    table.remove(tbl, 1)
-end
+	table.remove(tbl, 1)
+end;
 table.pop = function(tbl)
-    table.remove(tbl)
-end
+	table.remove(tbl)
+end;
 table.map = function(sequence, transformation)
-    local newlist = {}
-    do
-        local i = 0
-        while i < #sequence do
-            table.insert(newlist, transformation(sequence[i + 1]))
-            local _null
-            (function()
-                i = i + 1
-                return i
-            end)()
-        end
-    end
-    return newlist
-end
+	local newlist = {};
+	do
+		local i = 0
+		while i < # sequence do
+			table.insert(newlist, transformation(sequence[i + 1]));
+			local _null
+			i = i + 1
+		end
+	end;
+	return newlist
+end;
 table.filter = function(sequence, predicate)
-    local newlist = {}
-    do
-        local i = 0
-        while i < #sequence do
-            if predicate(sequence[i + 1]) then
-                table.insert(newlist, sequence[i + 1])
-            end
-            (function()
-                i = i + 1
-                return i
-            end)()
-        end
-    end
-    return newlist
-end
+	local newlist = {};
+	do
+		local i = 0
+		while i < # sequence do
+			if predicate(sequence[i + 1]) then
+				table.insert(newlist, sequence[i + 1])
+			end
+			i = i + 1
+		end
+	end;
+	return newlist
+end;
 table.reduce = function(sequence, operator)
-    if #sequence == 0 then
-        return nil
-    end
-    local out = nil
-    do
-        local i = 0
-        while i < #sequence do
-            out = operator(out, sequence[i + 1])
-            local _null
-            (function()
-                i = i + 1
-                return i
-            end)()
-        end
-    end
-    return out
+	if # sequence == 0 then
+		return nil
+	end;
+	local out = nil;
+	do
+		local i = 0
+		while i < # sequence do
+			out = operator(out, sequence[i + 1]);
+			local _null
+			i = i + 1
+		end
+	end;
+	return out
 end
-
